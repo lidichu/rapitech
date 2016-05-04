435b33a9f484a0ce441b267c7074651cf054b45c
lidichu <lidichu@gmail.com>
2016-05-04 17:19:17 +0800

    d

--- manager/inc/Connection.php
+++ manager/inc/Connection.php
@@ -3,7 +3,7 @@ session_start();
 /*資料庫連結設定*/
 date_default_timezone_set("Asia/Taipei");
 //$Conn = mysql_connect('localhost','root','2776');
-$Conn = mysql_connect('localhost','motherwang_admin','2604222');
+$Conn = mysql_connect('localhost','rapitech','');
 mysql_select_db("motherwang",$Conn);
 mysql_query("SET NAMES 'utf8'");
 header("Cache-control: private"); //防止網頁過期
