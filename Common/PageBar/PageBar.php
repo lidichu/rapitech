927faae3758b9742e3813b828c9664b0850c4ba7
silvia <silvia@atinfo.com.tw>
2016-09-11 01:11:41 +0800

    修改分頁處理問題

--- Common/PageBar/PageBar.php
+++ Common/PageBar/PageBar.php
@@ -15,17 +15,17 @@
 <div class="text-center ">
 	<ul class="pagination">
 		<?php if($Page>1){ ?>
-			<li><a href="<?php echo GetScriptName()."?Page=".($Page - 1).$OtherQuery ?>" aria-label="Previous" title="上一頁,第<?php echo($Page - 1)?>頁"> <span aria-hidden="true">&laquo;</span></a></li>
+			<li><a href="<?php echo GetScriptName(). "?G0=" . $G0 . "&Page=".($Page - 1).$OtherQuery ?>" aria-label="Previous" title="上一頁,第<?php echo($Page - 1)?>頁"> <span aria-hidden="true">&laquo;</span></a></li>
 		<?php }?>
 		<?php for ($i=$StartPage;$i<=$EndPage;$i++){?>
 			<?php if($i!=$Page){?>
-				<li><a href="<?php echo GetScriptName() . "?Page=" . $i . $OtherQuery ?>" title="第<?php echo $i?>頁"><?php echo $i?></a></li>
+				<li><a href="<?php echo GetScriptName() . "?G0=" . $G0 . "&Page=" . $i . $OtherQuery ?>" title="第<?php echo $i?>頁"><?php echo $i?></a></li>
 			<?php }else{?>
 				<li class="active"><a href="#" title="第 <?php echo $i?> 頁,目前所在頁"><?php echo $i?></a></li>
 			<?php }?>
 		<?php }?>
 		<?php if ($Page < $PageAmount ){?>
-			<li><a href="<?php echo GetScriptName()."?Page=".($Page + 1).$OtherQuery ?>" title="下一頁,第<?php echo($Page + 1)?>頁" aria-label="Next"> <span aria-hidden="true">&raquo;</span>
+			<li><a href="<?php echo GetScriptName(). "?G0=" . $G0 . "&Page=".($Page + 1).$OtherQuery ?>" title="下一頁,第<?php echo($Page + 1)?>頁" aria-label="Next"> <span aria-hidden="true">&raquo;</span>
 		<?php }?>
 	</ul>
 </div>
